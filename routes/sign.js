bdd2d1ec095d8bdc20f1eb661071469cff48e8d8
宝华 <baohua@alightmedia.cn>
2017-09-03 13:03:05 +0800

    手动合并signjs

--- routes/sign.js
+++ routes/sign.js
@@ -2,21 +2,21 @@
  * Created by baohua on 17/5/10.
  */
 var express = require("express"),
-    router = express.Router(),
-    assert = require('assert');
+router = express.Router(),
+assert = require('assert');
 var crypto = require('crypto'),
-    User = require('../models/user');
+User = require('../models/user');
 
 var isEmptyObj = function(obj) {
-  for(key in obj){
-    return false;
-  }
-  return true;
+for(key in obj){
+return false;
+}
+return true;
 };
 
-router.get('/in', function(req, res) {
-  res.render('in', {
-    title: 'Sign In'
+router.get('(/in)?', function(req, res) {
+res.render('in', {
+  title: 'Sign In'
   })
 });
 
@@ -28,7 +28,7 @@ router.get('/out', function(req, resp){
 router.get('/save', function(req, resp) {
   var data = req.query;
   var md5 = crypto.createHash('md5'),
-    pwd = md5.update(String(data.pwd)).digest('hex');
+  pwd = md5.update(String(data.pwd)).digest('hex');
 
   var user = new User({user: String(data.user), pwd: pwd});
   user.save(function(err, docs) {
@@ -42,19 +42,17 @@ router.post('/aIn', function(req, res) {
   console.log(data);
   var userModule = new User();
   userModule.get(data.user, function(err, docs) {
-    var md5 = crypto.createHash('md5'),
-      pwd = md5.update(String(data.pwd)).digest('hex');
+  var md5 = crypto.createHash('md5'),
+  pwd = md5.update(String(data.pwd)).digest('hex');
 
-      console.log(1231232);
+  const isPass = !isEmptyObj(docs) && docs.pwd === pwd;
 
-    const isPass = !isEmptyObj(docs) && docs.pwd === pwd;
-
-    if(isPass){
-      req.session.userinfo = { name: data.user };
-      res.json({ status: 0, msg: 'pass' });
-    }else {
-      res.json({status: 1, msg: '账号或密码有误！'});
-    }
+  if(isPass){
+    req.session.userinfo = { name: data.user };
+    res.json({ status: 0, msg: 'pass' });
+  }else {
+    res.json({status: 1, msg: '账号或密码有误！'});
+  }
     res.end();
   })
 });
